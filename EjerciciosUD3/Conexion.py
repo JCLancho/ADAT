b977f86a131e0c53efafdf260abcd420b90ce87c
JCLancho <jclancho0@gmail.com>
2019-11-05 13:28:19 +0100

    ud03

---
+++ EjerciciosUD3/Conexion.py
@@ -0,0 +1,37 @@
+import mysql.connector
+from mysql.connector import Error
+
+
+class Conexion:
+
+    def __init__(self):
+        try:
+            connection = mysql.connector.connect(host='localhost',
+                                                 user='dm2',
+                                                 password='dm2')
+
+            if connection.is_connected():
+                db_Info = connection.get_server_info()
+                print("Connected to MySQL Server version ", db_Info)
+                cursor = connection.cursor()
+                cursor.execute("select database();")
+                record = cursor.fetchone()
+                print("You're connected to database: ", record)
+                cursor.execute("CREATE DATABASE mydatabase")
+                connection.commit()
+                connection = mysql.connector.connect(host='localhost',
+                                                 user='dm2',
+                                                 password='dm2',
+                                                 database='mydatabase')
+                cursor = connection.cursor()
+                cursor.execute("select database();")
+                record = cursor.fetchone()
+                print("You're connected to database: ", record)
+
+        except Error as e:
+            print("Error while connecting to MySQL", e)
+        finally:
+            if connection.is_connected():
+                cursor.close()
+                connection.close()
+                print("MySQL connection is closed")
--- EjerciciosUD3/Olimpiadas.py
+++ EjerciciosUD3/Olimpiadas.py
@@ -1,24 +1,55 @@
-import mysql.connector
-from mysql.connector import Error
+from EjerciciosUD3.Conexion import Conexion
 
-try:
-    connection = mysql.connector.connect(host='localhost',
-                                         database='OLIMPIADAS',
-                                         user='dm2',
-                                         password='dm2')
 
-    if connection.is_connected():
-        db_Info = connection.get_server_info()
-        print("Connected to MySQL Server version ", db_Info)
-        cursor = connection.cursor()
-        cursor.execute("select database();")
-        record = cursor.fetchone()
-        print("You're connected to database: ", record)
+class Olimpiadas:
 
-except Error as e:
-    print("Error while connecting to MySQL", e)
-finally:
-    if connection.is_connected():
-        cursor.close()
-        connection.close()
-        print("MySQL connection is closed")
+    menu = """
+    ¿Qué desea hacer?
+        1.- Crear BBDD MySQL
+        2.- Crear BBDD SQLite
+        3.- Listado de deportistas en diferentes deportes
+        4.- Listado de deportistas participantes
+        5.- Modificar medalla deportista
+        6.- Añadir deportista/participación
+        7.- Eliminar participación
+        0.- Salir
+        """
+
+    def crearBbddMySql(self):
+        c = Conexion()
+
+    def crearBbddSqlite(self):
+        pass
+
+    def listadoDeportistasDiferentes(self):
+        pass
+
+    def listadoDeportistasParticipantes(self):
+        pass
+
+    def modificarMedallaDeportista(self):
+        pass
+
+    def anadirDeportistaParticipacion(self):
+        pass
+
+    def eliminarParticipacion(self):
+        pass
+
+
+o = Olimpiadas()
+options = {"1": o.crearBbddMySql,
+           "2": o.crearBbddSqlite,
+           "3": o.listadoDeportistasDiferentes,
+           "4": o.listadoDeportistasParticipantes,
+           "5": o.modificarMedallaDeportista,
+           "6": o.anadirDeportistaParticipacion,
+           "7": o.eliminarParticipacion
+           }
+opcion = input(o.menu)
+while opcion != "0":
+    try:
+        options[opcion]()
+    except KeyError:
+        print("Elige una opcion del menu")
+    opcion = input(o.menu)
